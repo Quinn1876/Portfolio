9ec018d080a92992f6cf8b8b3dfc3ec3c3a6d0ee
Quinn1876 <qhodges0442@gmail.com>
2019-09-07 14:39:41 -0400

    Added Routing and skeleton Pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Home from "./pages/home.page";
+import Contact from "./pages/contact.page";
+import Notfound from "./pages/notfound.page";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+  <Router>
+    <div>
+      {/* <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul> */}
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Redirect exact from="/" to="/home" />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById("root"));
